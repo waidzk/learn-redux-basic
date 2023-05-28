6eb0a4c3ff3d741349ddf95f23e43fb81464c885
waidzk <waidzk30@gmail.com>
2023-05-28 10:55:18 +0700

    Add deleteKontak

--- src/actions/kontakAction.js
+++ src/actions/kontakAction.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 export const GET_LIST_KONTAK = "GET_LIST_KONTAK";
 export const ADD_KONTAK = "ADD_KONTAK";
+export const DELETE_KONTAK = "DELETE_KONTAK";
 
 export const getListKontak = () => {
   console.log("2. Masuk ke kontak action");
@@ -19,7 +20,7 @@ export const getListKontak = () => {
     //GET API
     axios({
       method: "GET",
-      url: "http://localhost:3000/contacts",
+      url: "http://localhost:3001/contacts",
       timeout: 12000,
     })
       .then((response) => {
@@ -48,7 +49,6 @@ export const getListKontak = () => {
 };
 
 export const addKontak = (data) => {
-  console.log("2. Masuk ke kontak action");
   return (dispatch) => {
     //loading
     dispatch({
@@ -60,16 +60,15 @@ export const addKontak = (data) => {
       },
     });
 
-    //GET API
+    //ADD Data
     axios({
       method: "POST",
-      url: "http://localhost:3000/contacts",
+      url: "http://localhost:3001/contacts",
       timeout: 12000,
       data: data,
     })
       .then((response) => {
-        // BERHASIL GET API
-        console.log("3. Berhasil get data! : ", response);
+        // BERHASIL ADD Data
         dispatch({
           type: ADD_KONTAK,
           payload: {
@@ -80,10 +79,55 @@ export const addKontak = (data) => {
         });
       })
       .catch((error) => {
-        //Gagal GET API
-        console.log("3. gagal get data! : ", error);
+        //Gagal add Data
         dispatch({
           type: ADD_KONTAK,
+          payload: {
+            loading: false,
+            data: false,
+            errorMessage: error.message,
+          },
+        });
+      });
+  };
+};
+
+export const deleteKontak = (id) => {
+  console.log("2. Masuk ke kontak action");
+  return (dispatch) => {
+    //loading
+    dispatch({
+      type: DELETE_KONTAK,
+      payload: {
+        loading: true,
+        data: false,
+        errorMessage: false,
+      },
+    });
+
+    //GET API
+    axios({
+      method: "DELETE",
+      url: `http://localhost:3001/contacts/${id}`,
+      timeout: 12000,
+    })
+      .then((response) => {
+        // BERHASIL DELETE DATA
+        console.log("3. Berhasil delete data! : ", response);
+        dispatch({
+          type: DELETE_KONTAK,
+          payload: {
+            loading: false,
+            data: response.data,
+            errorMessage: false,
+          },
+        });
+      })
+      .catch((error) => {
+        //Gagal DELETE DATA
+        console.log("3. gagal delete data! : ", error);
+        dispatch({
+          type: DELETE_KONTAK,
           payload: {
             loading: false,
             data: false,
--- src/components/ListKontak/index.js
+++ src/components/ListKontak/index.js
@@ -1,25 +1,45 @@
 import React, { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getListKontak } from "../../actions/kontakAction";
+import { deleteKontak, getListKontak } from "../../actions/kontakAction";
 
 function ListKontak() {
-  const { getListKontakResult, getListKontakLoading, getListKontakError } =
-    useSelector((state) => state.KontakReducer);
+  const {
+    getListKontakResult,
+    getListKontakLoading,
+    getListKontakError,
+    deleteKontakResult,
+  } = useSelector((state) => state.KontakReducer);
   const dispatch = useDispatch();
+
   useEffect(() => {
     // Panggil action getListKontak
     dispatch(getListKontak());
   }, [dispatch]);
+
+  useEffect(() => {
+    if (deleteKontakResult) {
+      console.log("5. Masuk ComponentDidUpdate");
+      dispatch(getListKontak());
+    }
+  }, [deleteKontakResult, dispatch]);
 
   return (
     <div>
       <h4>List Kontak</h4>
       {getListKontakResult ? (
         getListKontakResult.map((contact) => (
-            <p key={contact.id}>{contact.name} | {contact.number}</p>
+          <p key={contact.id}>
+            {contact.name} | {contact.number} |
+            <button onClick={() => dispatch(deleteKontak(contact.id))}>
+              Hapus
+            </button>
+            {/* <button onClick={() => dispatch(detailKontak(contact))}>
+              Edit
+            </button> */}
+          </p>
         ))
       ) : getListKontakLoading ? (
-            <p>Loading ...</p>
+        <p>Loading ...</p>
       ) : (
         <p>{getListKontakError ? getListKontakError : "Data Kosong"}</p>
       )}
